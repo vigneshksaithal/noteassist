e623462c5245bb25bc304539567de51aea461039
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-09-12 10:45:23 +0530

    chore: Remove unnecessary files and update npm dependencies

--- .svelte-kit/generated/client-optimized/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.5.26_@sveltejs+vite-plugin-svelte@3.1.2_svelte@4.2.19_vite@5.4.4__svelte@4.2.19_vite@5.4.4/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
--- .svelte-kit/output/client/_app/immutable/entry/start.D-vcPZyu.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Chb3Yw-K.js";export{t as start};
