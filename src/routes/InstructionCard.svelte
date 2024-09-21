cb1cdf60b1f87ba861c091af5843826c7d5fc2da
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-09-21 12:14:32 +0530

    Update instructions spacing and font size

--- src/routes/InstructionCard.svelte
+++ src/routes/InstructionCard.svelte
@@ -6,23 +6,21 @@ import * as Card from '$lib/components/ui/card'
 	class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-xl"
 >
 	<Card.Header>
-		<Card.Title tag="h2">How to use Video 2 Notes?</Card.Title>
+		<Card.Title tag="h2" class="text-2xl">How to use Video 2 Notes?</Card.Title>
 	</Card.Header>
 	<Card.Content>
-		<ol class="list-decimal list-inside">
+		<ol class="list-decimal list-inside space-y-4">
 			<li>
-				0. Make sure you have given Highlight and this app the permission to use
+				Make sure you have given Highlight and this app the permission to use
 				audio.
 			</li>
 			<li>Click the <b>Start</b> button to begin recording.</li>
 			<li>
-				Watch/Play the video you want to take notes on. It may be YouTube, Zoom,
-				etc.
+				Go watch/play the video you want to take notes on. It may be YouTube,
+				Zoom, etc.
 			</li>
-			<li>Click "Stop" when you're done.</li>
-			<li>
-				Your note will be automatically generated and saved in NoteAssist.
-			</li>
+			<li>Then come back to this app and click "Stop" when you're done.</li>
+			<li>Your note will be automatically generated and saved.</li>
 		</ol>
 	</Card.Content>
 </Card.Root>
